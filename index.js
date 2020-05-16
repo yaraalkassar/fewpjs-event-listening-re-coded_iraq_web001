61de6c038897e534539875fd8371f59cfa913db8
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-16 01:26:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function addingEventListener() {
-  const input = document.getElementById('input');
+  const input = document.getElementById('input#input');
 input.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
